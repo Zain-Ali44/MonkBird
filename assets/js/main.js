8d4396d5824021f8dd97a7ae7696c72275ebbdaa
Zain-Ali44 <zain922ali@gmail.com>
2024-10-28 11:26:52 -0700

    add section

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// const cursor = document.querySelector(".cursor");
+
+// document.addEventListener("mousemove", customizeCursor);
+
+// function customizeCursor(e) {
+//   cursor.style.top = e.pageY + "px";
+//   cursor.style.left = e.pageX + "px";
+// }
